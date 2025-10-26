1b4a9ac6eb3aedb813234767777364c35045b591
Bruce Yu <bruceyu2008@gmail.com>
2025-10-26 11:55:21 +0800

    更新 vite.config.ts 以配合 GitHub repository 名稱

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace 'fraction2' with your actual repository name
-  base: '/fraction2/',
+  // IMPORTANT: Replace 'fracion_two' with your actual repository name
+  base: '/fracion_two/',
 })
